fe090e1252858efec79b378c29789d55413e6d28
Armando Rodriguez <armandorodriguezm1998@gmail.com>
2023-09-02 17:36:56 -0600

    keys arregladas

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,22 +7,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-
-console.log("apiKey", process.env.REACT_APP_FIREBASE_apiKey);
-console.log("authDomain", process.env.REACT_APP_FIREBASE_authDomain);
-console.log("projectID", process.env.REACT_APP_FIREBASE_projectId);
-console.log("storageBucket", process.env.REACT_APP_FIREBASE_storageBucket);
-console.log("messagingSenderId", process.env.REACT_APP_FIREBASE_messagingSenderId);
-console.log("appId", process.env.REACT_APP_FIREBASE_appId)
-
 // Obtén las credenciales de Firebase desde las variables de entorno
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
-    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
-    projectId: process.env.REACT_APP_FIREBASE_projectId,
-    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-    appId: process.env.REACT_APP_FIREBASE_appId,
+    apiKey: "AIzaSyCjs84uPckEVItrBMnwBihqeMR8jqteIvU",
+    authDomain: "entregafinalrodriguez-17e6d.firebaseapp.com",
+    projectId: "entregafinalrodriguez-17e6d",
+    storageBucket: "entregafinalrodriguez-17e6d.appspot.com",
+    messagingSenderId: "460125155215",
+    appId: "1:460125155215:web:777ae5a7b9547db92825fa",
 };
 
 // Inicializa Firebase
@@ -32,7 +24,21 @@ const db = getFirestore(app);
 
 export default app;
 /*
-N.B: 
+N.B: de momento .env manda demasiados errores en su deploy 
+(V.G: 
+<noscript>
+    You need to enable JavaScript to run this app.
+</noscript>)
+
+Por ende omito la variable de entrno pero, su respectiva declaración es:
+
+REACT_APP_FIREBASE_apiKey=AIzaSyCjs84uPckEVItrBMnwBihqeMR8jqteIvU
+REACT_APP_FIREBASE_authDomain=entregafinalrodriguez-17e6d.firebaseapp.com
+REACT_APP_FIREBASE_projectId=entregafinalrodriguez-17e6d
+REACT_APP_FIREBASE_storageBucket=entregafinalrodriguez-17e6d.appspot.com
+REACT_APP_FIREBASE_messagingSenderId=460125155215
+REACT_APP_FIREBASE_appId=1:460125155215:web:777ae5a7b9547db92825fa
+
 */
 
 
