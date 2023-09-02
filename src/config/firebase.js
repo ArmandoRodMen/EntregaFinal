5465dabb6db42bbb4afa369309da59ebe6008bb3
Armando Rodriguez <armandorodriguezm1998@gmail.com>
2023-09-02 17:30:06 -0600

    keys limpias

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,22 +7,34 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+
+console.log("apiKey", process.env.REACT_APP_FIREBASE_apiKey);
+console.log("authDomain", process.env.REACT_APP_FIREBASE_authDomain);
+console.log("projectID", process.env.REACT_APP_FIREBASE_projectId);
+console.log("storageBucket", process.env.REACT_APP_FIREBASE_storageBucket);
+console.log("messagingSenderId", process.env.REACT_APP_FIREBASE_messagingSenderId);
+console.log("appId", process.env.REACT_APP_FIREBASE_appId)
+
+// Obtén las credenciales de Firebase desde las variables de entorno
 const firebaseConfig = {
-    apiKey: "AIzaSyCjs84uPckEVItrBMnwBihqeMR8jqteIvU",
-    authDomain: "entregafinalrodriguez-17e6d.firebaseapp.com",
-    projectId: "entregafinalrodriguez-17e6d",
-    storageBucket: "entregafinalrodriguez-17e6d.appspot.com",
-    messagingSenderId: "460125155215",
-    appId: "1:460125155215:web:777ae5a7b9547db92825fa",
+    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE_appId,
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app); // Lllamado a Firestore
+const db = getFirestore(app);
 
-export default app; // Exporta la instancia inicializada de Firebase
+export default app;
+/*
+N.B: 
+*/
+
 
 /*
     Fin de código
